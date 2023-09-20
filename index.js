f4065f0e057e5ad7c2b8b6ce41fd8ad5035fdb39
sheilabett <sheilahbett@gmail.com>
2023-09-20 21:25:14 +0300

    Test

---
+++ index.js
@@ -0,0 +1,10 @@
+//sheshe
+
+const divide = (num1="400",num2="50") => num1 / num2;
+divide();
+
+const square = x => x*x;
+square();
+
+const add =( parameter1,parameter2) => parameter1 + parameter2;
+add(6,9);
